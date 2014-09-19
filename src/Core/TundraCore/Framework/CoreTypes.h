ebf3aac180143a4801316c84483eba3f431b1d0d
Stinkfist0 <stinkfist0@gmail.com>
2014-09-19 15:40:51 +0300

    CoreTypes.h cleanup.

--- src/Core/TundraCore/Framework/CoreTypes.h
+++ src/Core/TundraCore/Framework/CoreTypes.h
@@ -2,19 +2,6 @@
 
 #pragma once
 
-// types
-
-#include <vector>
-#include <list>
-
-#if defined(unix) || defined(__APPLE__)
-#include <cmath>
-#include <limits>
-// Gnu GCC has C99-standard macros as an extension but in some system there does not exist them so we define them by ourself.
-template <class T> inline bool _finite(const T &f) { return f != std::numeric_limits<T>::infinity(); }
-template <class T> inline bool _isnan(const T &f) { return f != f; }
-#endif
-
 // Use kNet/Types.h for fixed-width types instead of duplicating the code here.
 #include <kNet/Types.h>
 
@@ -31,4 +18,3 @@ typedef unsigned long ulong;
 // Special Tundra identifiers
 typedef unsigned int entity_id_t;
 typedef unsigned int component_id_t;
-
