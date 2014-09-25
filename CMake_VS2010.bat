bf5c1bda8a4c9eb688d28009a379ba328411bf49
Stinkfist0 <stinkfist0@gmail.com>
2014-09-25 18:22:56 +0300

    Fix CMake batch files.

--- CMake_VS2010.bat
+++ CMake_VS2010.bat
@@ -1,5 +1,5 @@
 @echo off
-IF EXIST build\tundra-urho3d.sln del /Q build\tundra-urho3d.sln
+IF EXIST build-vs2010-x86\tundra-urho3d.sln del /Q build-vs2010-x86\tundra-urho3d.sln
 cd tools\Windows\
 call RunCMake "Visual Studio 10"
 cd ..\..
--- CMake_VS2013_X64.bat
+++ CMake_VS2013_X64.bat
@@ -1,5 +1,5 @@
 @echo off
-IF EXIST build\tundra-urho3d.sln del /Q build\tundra-urho3d.sln
+IF EXIST build-vs2013-x64\tundra-urho3d.sln del /Q build-vs2013-x64\tundra-urho3d.sln
 cd tools\Windows\
 call RunCMake "Visual Studio 12 Win64"
 cd ..\..
