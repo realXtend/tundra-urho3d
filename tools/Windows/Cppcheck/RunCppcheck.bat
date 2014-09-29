2f2d6fd660def24fede408fd931d934fff6b52cc
Stinkfist0 <stinkfist0@gmail.com>
2014-09-29 14:52:34 +0300

    Cppcheck: suppress uninitMemberVar warning for JSON class.

--- tools/Windows/Cppcheck/RunCppcheck.bat
+++ tools/Windows/Cppcheck/RunCppcheck.bat
@@ -1,6 +1,9 @@
 :: Assumes that Cppcheck is installed in the default location or accessible via PATH by default.
-:: The warning unusedFunction: 'The function 'funcName' is never used.' is suppressed because as an SDK,
-:: not all of the functions are not used by the library itself.
+:: The following warnings are suppressed:
+:: - unusedFunction: suppressed because as an SDK, not all of the functions are not used by the library itself.
+:: In Suppressions.txt:
+:: - uninitMemberVar:TundraCore\JSON\JSON.cpp: 'data' member variable is left uninitialized intentionally.
+
 :: TODO missingInclude warning suppressed for now until deps directory is configured to be included.
 
 @echo off
@@ -13,7 +16,8 @@ IF ERRORLEVEL 1 GOTO NoCppcheck
 
 set SRC_DIR=../../../src/
 call cppcheck --template "{file}({line}): ({severity}) ({id}): {message}" -DTUNDRACORE_API= -I%SRC_DIR% ^
-    -rp=%SRC_DIR% --enable=all --suppress=unusedFunction --suppress=missingInclude %SRC_DIR%
+    -rp=%SRC_DIR% --enable=all --suppress=unusedFunction --suppress=missingInclude --suppressions-list=Suppressions.txt ^
+    %SRC_DIR%
 
 GOTO End
 
