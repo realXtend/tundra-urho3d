6b23a5472c2df22a8834116b36119a35e71767eb
Stinkfist0 <stinkfist0@gmail.com>
2014-09-30 18:49:48 +0300

    Allow passing parameters in cmake-vs*.bat.

--- cmake-vs2013-x64.bat
+++ cmake-vs2013-x64.bat
@@ -1,6 +1,6 @@
 @echo off
 IF EXIST build-vs2013-x64\tundra-urho3d.sln del /Q build-vs2013-x64\tundra-urho3d.sln
 cd tools\Windows\
-call RunCMake "Visual Studio 12 Win64"
+call RunCMake "Visual Studio 12 Win64" %*
 cd ..\..
 pause
