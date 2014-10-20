dec59b3bddf8a6578a9ca12b7371e388f2b37b03
Stinkfist0 <stinkfist0@gmail.com>
2014-10-20 16:24:21 +0300

    Add StringCompareCaseInsensitive helper struct.

--- src/TundraCore/Framework/CoreStringUtils.h
+++ src/TundraCore/Framework/CoreStringUtils.h
@@ -10,6 +10,15 @@ namespace kNet { class DataSerializer; class DataDeserializer; }
 namespace Tundra
 {
 
+/// Can be used as a custom comparator in std::map or similar when wanting case-insensitive comparison.
+struct TUNDRACORE_API StringCompareCaseInsensitive
+{
+    bool operator()(const String &a, const String &b) const
+    {
+        return a.Compare(b, false) < 0;
+    }
+};
+
 /// Reads an UTF-8 encoded String from a data stream
 String TUNDRACORE_API ReadUtf8String(kNet::DataDeserializer &dd);
 
@@ -22,7 +31,7 @@ void TUNDRACORE_API WriteUtf8String(kNet::DataSerializer &ds, const String &str)
 void TUNDRACORE_API ParseCommand(String input, String &command, StringVector &parameters);
 
 /// Pad a string with whitespace.
-/** Negative @pad will insert the whitespace to the left, prositive to the right. */
+/** Negative @pad will insert the whitespace to the left, positive to the right. */
 String TUNDRACORE_API PadString(String str, int pad);
 
 }
