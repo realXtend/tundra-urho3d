48d77c73a1451864ac363c56d6116549db95a795
Lasse Öörni <lasse.oorni@ludocraft.com>
2014-10-22 14:59:24 +0300

    Unnecessary to set camera aspect ratio when (re)activating the camera.

--- src/Plugins/UrhoRenderer/Camera.cpp
+++ src/Plugins/UrhoRenderer/Camera.cpp
@@ -90,11 +90,6 @@ void Camera::SetActive()
     }
 
     world_.Lock()->Renderer()->SetMainCamera(ParentEntity());
-
-    /// @todo cadaver: probably no bugs with Urho here?
-    // Forcibly update the aspect ratio for the new camera. Ogre has a bug that activating a new camera will not automatically re-apply the aspect ratio automatically,
-    // if its setAutoAspectRatio was set to true. Therefore, re-apply the aspect ratio when activating a new camera to the main viewport.
-    SetAspectRatio(AspectRatio());
 }
 
 float Camera::AspectRatio() const
