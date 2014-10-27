a48e038c96b1e4c1f8e9bfb7097cbc1f7ed2ffe4
Jonne Nauha <jonne@adminotech.com>
2014-10-28 01:27:05 +0200

    ConfigTest Attempt to fix linux build.

--- tests/Config/TestConfig.cpp
+++ tests/Config/TestConfig.cpp
@@ -39,42 +39,47 @@ using namespace Tundra::Test;
 const ConfigData CfgWrite("Config Test", "Test Write");
 const ConfigData CfgWriteFile("Config Test", "Test Write File");
 
-namespace 
-{
-    // Note we use MGL Urho interop functionality as MLG has convinient support for random data.
-    static LCG Random;
+// Note we use MGL Urho interop functionality as MLG has convinient support for random data.
+static LCG Random;
 
-    /* @todo We would like to test negative values as well with Random.Int(Urho3D::M_MIN_INT, Urho3D::M_MAX_INT)
-       but it always returns -2147483648 or whatever the first param is? */
-    static int RandomInt()              { return Random.Int(); }
-    static float RandomFloat()          { return Random.Float(); }
-    static bool  RandomBool()           { return Random.Int(0,1) == 1; }
-
-    const Variant Value_VAR_INT         = RandomInt();
-    const Variant Value_VAR_BOOL        = RandomBool();
-    const Variant Value_VAR_FLOAT       = Random.Float();
-    const Variant Value_VAR_VECTOR2     = float2::RandomDir(Random);
-    const Variant Value_VAR_VECTOR3     = float3::RandomDir(Random);
-    const Variant Value_VAR_VECTOR4     = float4::RandomDir(Random);
-    const Variant Value_VAR_QUATERNION  = Quat::RandomRotation(Random);
-    const Variant Value_VAR_COLOR       = Urho3D::Color(RandomFloat(), RandomFloat(), RandomFloat(), RandomFloat());
-    const Variant Value_VAR_STRING      = "This [is] a = a test } { @ ?!";
-    // const Variant Value_VAR_BUFFER;
-    // const Variant Value_VAR_VOIDPTR;
-    // const Variant Value_VAR_RESOURCEREF = Urho3D::ResourceRef(StringHash("TestName"), "TestType");
-    // const Variant Value_VAR_RESOURCEREFLIST;
-    // const Variant Value_VAR_VARIANTVECTOR;
-    // const Variant Value_VAR_VARIANTMAP;
-    const Variant Value_VAR_INTRECT     = Urho3D::IntRect(RandomInt(), RandomInt(), RandomInt(), RandomInt());
-    const Variant Value_VAR_INTVECTOR2  = Urho3D::IntVector2(RandomInt(), RandomInt());
-    // const Variant Value_VAR_PTR;
-    const Variant Value_VAR_MATRIX3     = float3x3::RandomGeneral(Random, -RandomFloat(), RandomFloat());   
-    const Variant Value_VAR_MATRIX3X4   = float3x4::RandomGeneral(Random, -RandomFloat(), RandomFloat());
-    const Variant Value_VAR_MATRIX4     = float4x4::RandomGeneral(Random, -RandomFloat(), RandomFloat());
-}
+/* @todo We would like to test negative values as well with Random.Int(Urho3D::M_MIN_INT, Urho3D::M_MAX_INT)
+    but it always returns -2147483648 or whatever the first param is? */
+int   RandomInt()       { return Random.Int(); }
+float RandomFloat()     { return Random.Float(); }
+bool  RandomBool()      { return Random.Int(0,1) == 1; }
 
 VariantList DataList()
 {
+    static Urho3D::Vector2 v2           = float2::RandomDir(Random);
+    static Urho3D::Vector3 v3           = float3::RandomDir(Random);
+    static Urho3D::Vector4 v4           = float4::RandomDir(Random);
+    static Urho3D::Quaternion q         = Quat::RandomRotation(Random);
+    static Urho3D::Matrix3 m3           = float3x3::RandomGeneral(Random, -RandomFloat(), RandomFloat());
+    static Urho3D::Matrix3x4 m3x4       = float3x4::RandomGeneral(Random, -RandomFloat(), RandomFloat());
+    static Urho3D::Matrix4 m4           = float4x4::RandomGeneral(Random, -RandomFloat(), RandomFloat());
+
+    static Variant Value_VAR_INT        = RandomInt();
+    static Variant Value_VAR_BOOL       = RandomBool();
+    static Variant Value_VAR_FLOAT      = Random.Float();
+    Variant Value_VAR_VECTOR2           = v2;
+    Variant Value_VAR_VECTOR3           = v3;
+    Variant Value_VAR_VECTOR4           = v4;
+    Variant Value_VAR_QUATERNION        = q;
+    static Variant Value_VAR_COLOR      = Urho3D::Color(RandomFloat(), RandomFloat(), RandomFloat(), RandomFloat());
+    static Variant Value_VAR_STRING     = "This [is] a = a test } { @ ?!";
+    // Variant Value_VAR_BUFFER;
+    // Variant Value_VAR_VOIDPTR;
+    // Variant Value_VAR_RESOURCEREF = Urho3D::ResourceRef(StringHash("TestName"), "TestType");
+    // Variant Value_VAR_RESOURCEREFLIST;
+    // Variant Value_VAR_VARIANTVECTOR;
+    // Variant Value_VAR_VARIANTMAP;
+    static Variant Value_VAR_INTRECT    = Urho3D::IntRect(RandomInt(), RandomInt(), RandomInt(), RandomInt());
+    static Variant Value_VAR_INTVECTOR2 = Urho3D::IntVector2(RandomInt(), RandomInt());
+    // const Variant Value_VAR_PTR;
+    Variant Value_VAR_MATRIX3           = m3;
+    Variant Value_VAR_MATRIX3X4         = m3x4;
+    Variant Value_VAR_MATRIX4           = m4;
+
     VariantList data;
     data.Push(Value_VAR_INT);
     data.Push(Value_VAR_BOOL);
@@ -121,7 +126,7 @@ TEST_F(Runner, WriteConfig)
     for(auto iter=data.Begin(); iter!=data.End(); ++iter)
     {
         const String &key = iter->first_;
-        const Variant &value = iter->second_;         
+        const Variant &value = iter->second_;
         String expected = value.ToString();
 
         Log(PadString(static_cast<int>(value.GetType()), 3) + value.GetTypeName(), 2);
@@ -142,7 +147,7 @@ TEST_F(Runner, WriteConfig)
     for(auto iter=data.Begin(); iter!=data.End(); ++iter)
     {
         const String &key = iter->first_;
-        const Variant &value = iter->second_;         
+        const Variant &value = iter->second_;
         String expected = value.ToString();
 
         // Write to memory
@@ -167,7 +172,7 @@ TEST_F(Runner, ReadConfig)
     for(auto iter=data.Begin(); iter!=data.End(); ++iter)
     {
         const String &key = iter->first_;
-        const Variant &value = iter->second_;         
+        const Variant &value = iter->second_;
         String expected = value.ToString();
 
         {
@@ -191,7 +196,7 @@ TEST_F(Runner, ReadConfig)
     for(auto iter=data.Begin(); iter!=data.End(); ++iter)
     {
         const String &key = iter->first_;
-        const Variant &value = iter->second_;         
+        const Variant &value = iter->second_;
         String expected = value.ToString();
 
         {
