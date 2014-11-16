c3a29bd9e1675ac38cb6a15609ccffd5a5f34ea6
Jonne Nauha <jonne@adminotech.com>
2014-11-16 03:11:34 +0200

    Android cmake: Cleanup file and update to new style of passing dep locations to cmake instead of env variable. Added to-be-committed curl and zziplib.

--- cmake-android.bat
+++ cmake-android.bat
@@ -1,17 +1,29 @@
 @echo off
+
 set TUNDRA_DIR=%CD%
-set TUNDRA_DEP_PATH=%CD%\deps-android
-set URHO3D_HOME=%TUNDRA_DEP_PATH%\urho3d
-set MATHGEOLIB_HOME=%TUNDRA_DEP_PATH%\MathGeoLib\build
-set KNET_HOME=%TUNDRA_DEP_PATH%\kNet
+set DEPS=%CD%\deps-android
+
 cd src\android
-cmake -G "Unix Makefiles" -DANDROID=1 -DENABLE_TESTS=OFF -DCMAKE_TOOLCHAIN_FILE=%TUNDRA_DIR%\cmake\android.toolchain.cmake ../..
+
+cmake ../.. ^
+    -G "Unix Makefiles" -DANDROID=1 ^
+    -DCMAKE_TOOLCHAIN_FILE=%TUNDRA_DIR%\cmake\android.toolchain.cmake ^
+    -DURHO3D_HOME=%DEPS%\urho3d ^
+    -DMATHGEOLIB_HOME=%DEPS%\MathGeoLib\build ^
+    -DKNET_HOME=%DEPS%\kNet ^
+    -DGTEST_HOME=%DEPS%\gtest ^
+    -DCURL_HOME=%DEPS%\curl\build ^
+    -DZZIPLIB_HOME=%DEPS%\zziplib
+
 if ERRORLEVEL 0 goto Success
 goto End
+
 :Success
 cd ..\..
+echo.
 echo CMake for Android completed. Go to src\android directory and execute make 
 echo to build Tundra-urho3d. Then use eg. ant debug to package the apk. Refer 
 echo to README.md for details
 goto End
+
 :End
