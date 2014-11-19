03a8409fd9224b9080743efc1205c4f21dd92ef3
Lasse Öörni <lasse.oorni@ludocraft.com>
2014-11-19 16:52:31 +0200

    Copy also Scenes & Assets directories to apk.

--- src/Android/CopyData.bat
+++ src/Android/CopyData.bat
@@ -1,4 +1,8 @@
 md assets\Data
+md assets\Scenes
+md assets\Assets
 xcopy ..\..\Bin\Data\*.* assets\Data /S /E /C /Y
+xcopy ..\..\Bin\Scenes\*.* assets\Scenes /S /E /C /Y
+xcopy ..\..\Bin\Assets\*.* assets\Assets /S /E /C /Y
 xcopy ..\..\Bin\*.json assets /S /E /C /Y
 rd /S /Q assets\Data\Shaders\HLSL
