af480e8d9eb86b10ac7ea87aeaea4c04b239f371
Lasse Öörni <loorni@gmail.com>
2015-12-08 21:33:04 +0200

    Fix StdPtr.h file for GCC. Since the CMakeLists.txt already requires GCC 4.7 or greater, only use std::tr1 namespace on VS2010.

--- src/TundraCore/Framework/StdPtr.h
+++ src/TundraCore/Framework/StdPtr.h
@@ -4,9 +4,9 @@
 
 #include <memory>
 
-#if defined(_MSC_VER) && (_MSC_VER == 1500) || (defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ >= 2 && !defined(__APPLE__))
+#if defined(_MSC_VER) && (_MSC_VER == 1500) 
 #define STDTR1_NAMESPACE std::tr1
-#elif defined(_MSC_VER) && (_MSC_VER >= 1600) || (defined(__APPLE__) && defined(__clang__) && __clang_major__ == 4 && __clang_minor__ == 2) || (defined(__GNUC__) && __GNUC__ == 4 && __GNUC_MINOR__ >= 8 && !defined(__APPLE__))
+#else
 #define STDTR1_NAMESPACE std
 #endif
 
