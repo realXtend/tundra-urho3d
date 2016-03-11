a232f7d0c3bddddb9cb97105b341436e97074c86
Lasse Öörni <lasse.oorni@ludocraft.com>
2016-03-11 13:32:14 +0200

    Tidy up core bindings generation by generating the xml into the bindings source directory, not TundraCore source directory.

--- src/Plugins/JavaScript/BindingsGenerator/RegenerateCoreBindings.cmd
+++ src/Plugins/JavaScript/BindingsGenerator/RegenerateCoreBindings.cmd
@@ -3,8 +3,7 @@ setlocal EnableDelayedExpansion
 set TUNDRACORE_DIR=%CD%\..\..\..\TundraCore
 set ORIGINAL_DIR=%CD%
 
-copy %CD%\CoreBindings.doxyfile %TUNDRACORE_DIR%
 cd %TUNDRACORE_DIR%
-doxygen CoreBindings.doxyfile
+doxygen ..\Plugins\JavaScript\BindingsGenerator\CoreBindings.doxyfile
 cd %ORIGINAL_DIR%
-bin\release\BindingsGenerator.exe %TUNDRACORE_DIR%\CoreDocs\xml ../CoreBindings %TUNDRACORE_DIR% Scene Entity EntityAction IComponent Name DynamicComponent AttributeChange Framework FrameAPI SceneAPI ConfigAPI AssetAPI IAsset AssetReference AssetReferenceList Transform float3 float4 Quat float3x3 float3x4 float4x4
+bin\release\BindingsGenerator.exe ..\CoreBindings\CoreDocs\xml ..\CoreBindings %TUNDRACORE_DIR% Scene Entity EntityAction IComponent Name DynamicComponent AttributeChange Framework FrameAPI SceneAPI ConfigAPI AssetAPI IAsset AssetReference AssetReferenceList Transform float3 float4 Quat float3x3 float3x4 float4x4
