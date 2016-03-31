fd104b8913692e4c11479d762d3e372f6e6000a5
Lasse Öörni <lasse.oorni@ludocraft.com>
2016-03-31 11:33:55 +0300

    Add cmd file to regenerate all bindings.

---
+++ src/Plugins/RegenerateAllBindings.cmd
@@ -0,0 +1,21 @@
+set ORIGINAL_DIR=%CD%
+
+cd JavaScript\CoreBindings
+call RegenerateCoreBindings.cmd
+cd %ORIGINAL_DIR%
+
+cd JavaScript\JavaScriptBindings
+call RegenerateJavaScriptBindings.cmd
+cd %ORIGINAL_DIR%
+
+cd BulletPhysics\BulletPhysicsBindings
+call RegenerateBulletPhysicsBindings.cmd
+cd %ORIGINAL_DIR%
+
+cd UrhoRenderer\UrhoRendererBindings
+call RegenerateUrhoRendererBindings.cmd
+cd %ORIGINAL_DIR%
+
+cd TundraLogic\TundraLogicBindings
+call RegenerateTundraLogicBindings.cmd
+cd %ORIGINAL_DIR%
